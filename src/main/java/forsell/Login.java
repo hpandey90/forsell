6c291a80510f0141c2bfe7236411f34e9de91dde
ashish4feb <katiyar.ashish1989@gmail.com>
2016-12-24 02:01:29 -0500

    update Java files

--- src/main/java/forsell/Login.java
+++ src/main/java/forsell/Login.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpSession;
 
 public class Login extends HttpServlet{
 	
-	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 			
 		// get request parameters for userID and password
 				String user = request.getParameter("user");
